bc6d6a0fa4771e294aeb16e3e1a5ab076534d09e
Julien Andreu <julienandreu@me.com>
2024-10-02 07:46:19 -0400

    feat: Step 2

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,55 @@
-import { one } from "./one.ts";
+import * as one from "./one.ts";
+import * as two from "./two.ts";
 
 const input = { first: 1, second: 2 };
 
-const a = one.add(one.extract(input, "first"), one.extract(input, "second"));
+const flow_1 = (input): number[] => {
+    console.group("flow_1");
+    const a = one.add(one.extract(input, "first"), one.extract(input, "second"));
+    const b = one.multiply(a, a);
+    const c = one.multiply(b, one.side_effect(b));
+    const d = one.decompose(c);
+    const e = one.loop(d, one.add);
+    const final = e;
 
-const b = one.multiply(a, a);
+    console.log({
+        input,
+        a,
+        b,
+        c,
+        d,
+        e,
+        final,
+    }, { depth: null });
+    console.groupEnd();
 
-const c = one.multiply(b, one.side_effect(b));
+    return final;
+}
+const out_1 = flow_1(input);
 
-const d = one.decompose(c);
+const flow_2 = (input): number[] => {
+    console.group("flow_2");
 
-const e = one.loop(d, one.add);
+    const before_a_0 = two.from(input);
+    const before_a_1 = two.compose(before_a_0, two.extract("first"));
+    const before_a_2 = two.compose(before_a_0, two.extract("second"));
 
-const final = input;
+    const a = two.add(before_a_1(), before_a_2());
 
-console.dir({ input, a, b, c, d, e, final }, { depth: null });
+    const final = a();
+
+    console.log({
+        input,
+        ba0: before_a_0(),
+        ba1: before_a_1(),
+        ba2: before_a_2(),
+        a: a(),
+        final,
+    }, { depth: null });
+    console.groupEnd();
+
+    return input;
+}
+const out_2 = flow_2(input);
+
+console.dir({ out_1, out_2 }, { depth: null });
--- src/one.ts
+++ src/one.ts
@@ -1,29 +1,20 @@
-const add = (...input: number[]): number => input.reduce((acc, curr) => parseInt(acc.toString()) + parseInt(curr.toString()), 0);
+export const add = (...input: number[]): number => input.reduce((acc, curr) => parseInt(acc.toString()) + parseInt(curr.toString()), 0);
 
-const multiply = (...input: number[]): number => input.reduce((acc, curr) => parseInt(acc.toString()) * parseInt(curr.toString()), 1);
+export const multiply = (...input: number[]): number => input.reduce((acc, curr) => parseInt(acc.toString()) * parseInt(curr.toString()), 1);
 
-const side_effect = (input: number): number => {
+export const side_effect = (input: number): number => {
     const timestamp = Date.now();
     const last = parseInt(timestamp.toString().slice(-1));
     console.log("Side effect", last);
     return input + last;
 }
 
-const decompose = (input: number): number[] => input.toString().split("").map(Number);
+export const decompose = (input: number): number[] => input.toString().split("").map(Number);
 
-const extract = <T extends Record<string, any>>(source: T, key: keyof T): T[keyof T] => {
+export const extract = <T extends Record<string, any>>(source: T, key: keyof T): T[keyof T] => {
     return source[key];
 }
 
-const loop = <T extends (index: number, item: number) => ReturnType<T>>(input: number[], fn: T): ReturnType<T>[] => {
+export const loop = <T extends (index: number, item: number) => ReturnType<T>>(input: number[], fn: T): ReturnType<T>[] => {
     return input.map(fn);
-}
-
-export const one = {
-    add,
-    multiply,
-    side_effect,
-    decompose,
-    extract,
-    loop,
 }
