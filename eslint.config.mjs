f32e8f82617e073988fe3615d1f610806600523d
Julien Andreu <julienandreu@me.com>
2025-03-19 11:11:31 -0400

    chore: Setup repository

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    {
+        ignores: [
+            'eslint.config.mjs',
+            'dist',
+            'build',
+            'node_modules',
+        ],
+    }
+);
